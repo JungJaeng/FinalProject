b9766e8b206087380dfabdcad57e525432a82491
Hyunsub <coy10@naver.com>
2019-07-18 10:46:01 +0900

    123

--- FinalProject/src/main/java/web/service/impl/MainServiceImpl.java
+++ FinalProject/src/main/java/web/service/impl/MainServiceImpl.java
@@ -1,5 +1,6 @@
 package web.service.impl;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,19 @@ import web.service.face.MainService;
 public class MainServiceImpl implements MainService{
 	private static final Logger logger = LoggerFactory.getLogger(MainServiceImpl.class);
 	 
+	private static String Request_URL = "http://api.visitkorea.or.kr/openapi/service/rest/KorService/searchKeyword?";
+	private static String KEY = "serviceKey=oVpLLrYOZ3HgAbsrCL7WWVWeyhyK8sMPeScf3RiLkxYxnUSCCgj9rb8kRVW2MXInXYI3sSaZovOLHgmfur2rRg%3D%3D";
+	
+	public void result() {
+	
+	String strURL = null; //보낼 주소 URL
+	strURL = Request_URL;
+	strURL += KEY;
+	strURL += "&numOfRows=12";
+	strURL += "&pageNo=1";
+	strURL += "&type=json";
 	
 	
+	
+	}
 }	
